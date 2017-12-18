f8372294377354aa064d1957499b9d9c6e11827e
Yuki Sumida <y.sumida.0201@gmail.com>
2017-12-18 23:04:55 +0900

    とりあえず縦に並べる
    TODO 文字の下マージンが思ったとおりにならない

--- VerticalLayoutButton/VerticalLayoutButton.swift
+++ VerticalLayoutButton/VerticalLayoutButton.swift
@@ -16,4 +16,27 @@ class VerticalLayoutButton: UIButton {
 
         return CGSize(width: adjustedWidth, height: adjustedHeight)
     }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        self.contentVerticalAlignment = .top
+        self.contentHorizontalAlignment = .left
+        // アイコンとラベルを縦に並べてセンタリング
+        self.titleLabel?.sizeToFit()
+        let labelWidth = self.titleLabel!.frame.size.width
+        let labelHeight = self.titleLabel!.frame.size.height
+        let imageWidth = self.imageView?.frame.size.width ?? 0
+        let imageHeight = self.imageView?.frame.size.height ?? 0
+
+        // imageEdgeInsets
+        self.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 0, right: 0)
+
+        // titleEdgeInsets
+        self.titleEdgeInsets = UIEdgeInsets(top: imageHeight + 8, left: -1.0 * imageWidth, bottom: 0, right: 0)
+
+        // contentEdgeInsets
+        let bottomInset = (labelHeight + imageHeight) - rect.height
+        self.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: bottomInset, right: -1.0 * (rect.size.width - labelWidth))
+    }
 }
--- VerticalLayoutButton/ViewController.swift
+++ VerticalLayoutButton/ViewController.swift
@@ -13,11 +13,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        button.contentVerticalAlignment = .top
-        button.contentHorizontalAlignment = .left
-        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 0, bottom: 10, right: -8)
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 30, bottom: 0, right: 0)
-        button.titleEdgeInsets = UIEdgeInsets(top: 20, left: -20, bottom: 0, right: 0)
     }
 
     override func didReceiveMemoryWarning() {
