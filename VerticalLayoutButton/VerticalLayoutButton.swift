248c0a76ff40ae382376243e29d5cfa3ff6831e0
Yuki Sumida <y.sumida.0201@gmail.com>
2017-12-19 22:55:20 +0900

    とりあえず、左寄せ縦並びにする

--- VerticalLayoutButton/VerticalLayoutButton.swift
+++ VerticalLayoutButton/VerticalLayoutButton.swift
@@ -9,14 +9,6 @@
 import UIKit
 
 class VerticalLayoutButton: UIButton {
-    override var intrinsicContentSize: CGSize {
-        let size = super.intrinsicContentSize
-        let adjustedWidth = size.width + contentEdgeInsets.left + contentEdgeInsets.right
-        let adjustedHeight = size.height + contentEdgeInsets.top + contentEdgeInsets.bottom
-
-        return CGSize(width: adjustedWidth, height: adjustedHeight)
-    }
-
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
@@ -30,10 +22,10 @@ class VerticalLayoutButton: UIButton {
         let imageHeight = self.imageView?.frame.size.height ?? 0
 
         // imageEdgeInsets
-        self.imageEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 0, right: 0)
+        self.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
 
         // titleEdgeInsets
-        self.titleEdgeInsets = UIEdgeInsets(top: imageHeight + 8, left: -1.0 * imageWidth, bottom: 0, right: 0)
+        self.titleEdgeInsets = UIEdgeInsets(top: imageHeight, left: -1.0 * imageWidth, bottom: 0, right: 0)
 
         // contentEdgeInsets
         let bottomInset = (labelHeight + imageHeight) - rect.height
