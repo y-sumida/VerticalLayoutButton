0d6a5956638e63af7ccb948553ae07d7d67c471f
Yuki Sumida <y.sumida.0201@gmail.com>
2018-01-09 21:57:21 +0900

    Remove unnecessary custom initializer.

--- VerticalLayoutButton/VerticalLayoutButton.swift
+++ VerticalLayoutButton/VerticalLayoutButton.swift
@@ -40,14 +40,6 @@ class VerticalLayoutButton: UIButton {
         }
     }
 
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-    }
-
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
