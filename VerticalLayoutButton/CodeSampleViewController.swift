ff5c5a9f47a39156ac6f012fd8648df8e47ad489
Yuki Sumida <y.sumida.0201@gmail.com>
2018-01-17 23:34:07 +0900

    Add sample button2-6 for code.

--- VerticalLayoutButton/CodeSampleViewController.swift
+++ VerticalLayoutButton/CodeSampleViewController.swift
@@ -26,6 +26,44 @@ class CodeSampleViewController: UIViewController {
         button1.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         button1.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 42).isActive = true
 
+        let button2 = createButton(label: "B", fontSize: 15.0)
+        self.view.addSubview(button2)
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        button2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button2.topAnchor.constraint(equalTo: button1.bottomAnchor, constant: 20).isActive = true
+
+        let button3 = createButton(label: "Button", fontSize: 9.0)
+        self.view.addSubview(button3)
+        button3.translatesAutoresizingMaskIntoConstraints = false
+        button3.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button3.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: 20).isActive = true
+
+        let button4 = createButton(label: "Button", fontSize: 15.0)
+        self.view.addSubview(button4)
+        button4.translatesAutoresizingMaskIntoConstraints = false
+        button4.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button4.topAnchor.constraint(equalTo: button3.bottomAnchor, constant: 20).isActive = true
+        button4.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 30).isActive = true
+        button4.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -30).isActive = true
+
+        let button5 = createButton(label: "Button", fontSize: 15.0)
+        self.view.addSubview(button5)
+        button5.translatesAutoresizingMaskIntoConstraints = false
+        button5.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button5.topAnchor.constraint(equalTo: button4.bottomAnchor, constant: 20).isActive = true
+        button5.widthAnchor.constraint(equalToConstant: 30).isActive = true
+
+        let button6 = createButton(image: nil)
+        self.view.addSubview(button6)
+        button6.translatesAutoresizingMaskIntoConstraints = false
+        button6.topAnchor.constraint(equalTo: button5.bottomAnchor, constant: 20).isActive = true
+        button6.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 164).isActive = true
+
+        let button7 = createButton(label: nil)
+        self.view.addSubview(button7)
+        button7.translatesAutoresizingMaskIntoConstraints = false
+        button7.topAnchor.constraint(equalTo: button6.bottomAnchor, constant: 20).isActive = true
+        button7.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor, constant: 164).isActive = true
     }
 
     func createButton(label: String?="Button", fontSize: CGFloat=15, image:String?="setting") -> VerticalLayoutButton {
