6cf4a5b73a674009ff7e3fa2787c2ef0130f2159
Yuki Sumida <y.sumida.0201@gmail.com>
2018-01-17 23:33:07 +0900

    Add sample button for code.

--- VerticalLayoutButton/CodeSampleViewController.swift
+++ VerticalLayoutButton/CodeSampleViewController.swift
@@ -12,9 +12,38 @@ class CodeSampleViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSampleButtons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+
+    func addSampleButtons() {
+        let button1 = createButton(label: "Button", fontSize: 20.0)
+        self.view.addSubview(button1)
+        button1.translatesAutoresizingMaskIntoConstraints = false
+        button1.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button1.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 42).isActive = true
+
+    }
+
+    func createButton(label: String?="Button", fontSize: CGFloat=15, image:String?="setting") -> VerticalLayoutButton {
+        let defaultColor = UIColor(red: 0, green: 0.48, blue: 1, alpha: 1)
+        let hilightedColor = UIColor(red: 0, green: 0.48, blue: 1, alpha: 0.4)
+        let button = VerticalLayoutButton()
+        if let label = label {
+            button.setTitle(label, for: .normal)
+            button.titleLabel?.font = UIFont.systemFont(ofSize: fontSize)
+            button.setTitleColor(defaultColor, for: .normal)
+            button.setTitleColor(hilightedColor, for: .highlighted)
+        }
+        if let image = image {
+            button.setImage(UIImage(named: image)?.withRenderingMode(.alwaysTemplate), for: .normal)
+            button.tintColor = defaultColor
+        }
+        button.backgroundColor = UIColor(red: 0.84, green: 0.84, blue: 0.84, alpha: 1)
+
+        return button
     }
 }
